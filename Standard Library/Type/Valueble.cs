d1e34631c6d5e897910c7f252ff7ddb688c5fe53
LloydLion <LloydLion2211@gmlai.com>
2020-05-31 14:53:44 +0300

    Valueble

---
+++ Standard Library/Type/Valueble.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Standard_Library.Type
+{
+    public class Valueble<T> where T : class, ICloneable
+    {
+        private Valueble() { }
+
+        private T Reference { get; set; }
+        public T Value { get => this; }
+
+        public static implicit operator T(Valueble<T> a)
+        {
+            return (T)a.Reference.Clone();
+        }
+
+        public static implicit operator Valueble<T>(T a)
+        {
+            return new Valueble<T> { Reference = a };
+        }
+    }
+}
