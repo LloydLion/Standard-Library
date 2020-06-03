ba1879669cc3c65d5ebcbcee41297f6d3c6b9008
LloydLion <LloydLion2211@gmail.com>
2020-06-03 22:08:14 +0300

    ConsoleProcessEngine Start (uncomplited)

---
+++ Standard Library/ProcessManagment/ConsoleProcessEngine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Standard_Library.ProcessManagment
+{
+    public class ConsoleProcessEngine
+    {
+        private ProcessStartInfo processStartInfo;
+
+        public ConsoleProcessEngine(ProcessStartInfo processStartInfo)
+        {
+            this.processStartInfo = processStartInfo;
+        }
+
+        public string ReadConsoleSegment(string start, string end)
+        {
+
+        }
+    }
+}
