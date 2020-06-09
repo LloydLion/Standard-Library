bad40378fe816dcd0427d0ef0d68030ce314c4de
LloydLion <LloydLion2211@gmail.com>
2020-06-09 18:37:32 +0300

    ConsoleArgumentEngine improve

--- Standard Library/Console/ConsoleArgumentEngine.cs
+++ Standard Library/Console/ConsoleArgumentEngine.cs
@@ -7,9 +7,8 @@ namespace StandardLibrary.Console
 {
     public class ConsoleArgumentEngine
     {
-        public const string ParameterMemberHasInvalidTypeExcceptionMessage = 
-            "Parameter member has invalid type";
-
+        public const string ParameterMemberIsInvalid = 
+            "Parameter member is invalid";
 
         public const string ParameterIsNotHasAllKeysExcceptionMessage =
             "Parameter is not has all keys";
@@ -17,6 +16,17 @@ namespace StandardLibrary.Console
         public const string ParameterIsNotHasAllPositionParametersExcceptionMessage =
             "Parameter is not has all position parameters";
 
+        public const string SomePositionParametersHasInvalidPosition =
+            "Some position parameters has invalid position";
+
+        public const string SomeKeyParametersHasInvalidKey =
+            "Some key parameters has invalid key";
+        
+        public const string SomeFlagParametersHasInvalidKey =
+            "Some flag parameters has invalid key";
+
+        public const string SomeParametersHasInvalidType =
+            "Some parameters has invalid type";
 
 
         private readonly PositionParameter[] positions;
@@ -34,9 +44,60 @@ namespace StandardLibrary.Console
             keys = parameters.Where((s) => s.GetType() == typeof(KeyParameter))
                 .Cast<KeyParameter>().ToArray();
 
+            #region Validation
+
             if (positions.Length + flags.Length + keys.Length != parameters.Length)
                 throw new ArgumentException(nameof(parameters),
-                    ParameterMemberHasInvalidTypeExcceptionMessage);
+                    ParameterMemberIsInvalid,
+                    new Exception(SomeParametersHasInvalidType));
+
+            if (positions != null)
+            {
+                var tmp = positions.Select((s) => s.Position);
+                if (!Array.TrueForAll(positions, (s) => tmp.Count((q) => s.Position == q) == 1))
+                {
+                    throw new ArgumentException(nameof(parameters),
+                        ParameterMemberIsInvalid,
+                        new ArgumentException(SomePositionParametersHasInvalidPosition, "Position"));
+                }
+            }
+
+            if (keys != null)
+            {
+                var tmp = keys.Select((s) => s.Key);
+                if (!Array.TrueForAll(keys, (s) => tmp.Count((q) => s.Key == q) == 1))
+                {
+                    throw new ArgumentException(nameof(parameters),
+                        ParameterMemberIsInvalid,
+                        new ArgumentException(SomeKeyParametersHasInvalidKey, "Key"));
+                }
+            }
+            
+            if (flags != null)
+            {
+                var tmp = flags.Select((s) => s.Key);
+                if (!Array.TrueForAll(flags, (s) => tmp.Count((q) => s.Key == q) == 1))
+                {
+                    throw new ArgumentException(nameof(parameters),
+                        ParameterMemberIsInvalid,
+                        new ArgumentException(SomeFlagParametersHasInvalidKey, "Key"));
+                }
+            }
+
+            if(flags != null && keys != null)
+            {
+                var tmp1 = flags.Select((s) => s.Key).ToArray();
+                var tmp2 = keys.Select((s) => s.Key).ToArray();
+
+                if (!Array.TrueForAll(tmp1, (s) => !tmp2.Contains(s)))
+                {
+                    throw new ArgumentException(nameof(parameters),
+                        ParameterMemberIsInvalid,
+                        new ArgumentException(SomeFlagParametersHasInvalidKey, "Key"));
+                }
+            }
+
+            #endregion
         }
 
         public Dictionary<string, object> Calculate(string[] args)
@@ -44,35 +105,7 @@ namespace StandardLibrary.Console
             var dic = new Dictionary<string, object>();
             List<int> indexL = new List<int>();
 
-            try
-            {    
-                dic = dic.Concat(positions.Select((s, i) =>
-                {
-                    if (args.Length - 1 >= s.Position)
-                    {
-
-                        return new KeyValuePair<string, object>
-                            (s.Name, s.ParseFunc(args[s.Position]));
-                    }
-                    else
-                    {
-
-                        if (s.IsRequired)
-                            throw new IndexOutOfRangeException("Trigger exception");
-                        else
-                        {
-                            indexL.Add(i);
-                            return default;
-                        }
-                    }
-                })).Where((s, i) => !indexL.Contains(i))
-                .ToDictionary((s) => s.Key, (s) => s.Value);
-            }
-            catch (IndexOutOfRangeException)
-            {
-                throw new ArgumentException(nameof(args), 
-                    ParameterIsNotHasAllPositionParametersExcceptionMessage);
-            }
+            List<int> usedIndexes = new List<int>();
 
             try
             {
@@ -91,6 +124,8 @@ namespace StandardLibrary.Console
                     }
                     else
                     {
+                        usedIndexes.Add(i);
+
                         return new KeyValuePair<string, object>(s.Name, s.ParseFunc
                             (args[args.ToList().IndexOf("--" + s.Key) + 1]));
                     }
@@ -103,9 +138,41 @@ namespace StandardLibrary.Console
                 throw new ArgumentException(nameof(args), ParameterIsNotHasAllKeysExcceptionMessage);
             }
 
+            try
+            {
+                dic = dic.Concat(positions.Select((s, i) =>
+                {
+                    if (args.Length - 1 >= s.Position)
+                    {
+                        usedIndexes.Add(i);
+
+                        return new KeyValuePair<string, object>
+                            (s.Name, s.ParseFunc(args[s.Position]));
+                    }
+                    else
+                    {
+
+                        if (s.IsRequired)
+                            throw new IndexOutOfRangeException("Trigger exception");
+                        else
+                        {
+                            indexL.Add(i);
+                            return default;
+                        }
+                    }
+                })).Where((s, i) => !indexL.Contains(i) && !usedIndexes.Contains(i))
+                .ToDictionary((s) => s.Key, (s) => s.Value);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw new ArgumentException(nameof(args),
+                    ParameterIsNotHasAllPositionParametersExcceptionMessage);
+            }
+
             dic = dic.Concat
                 (flags.Select((s) => new KeyValuePair<string, object>
-                (s.Name, args.Contains("--" + s.Key))))
+                (s.Name, args.Contains("--" + s.Key)))
+                .Where((s, i) => usedIndexes.Contains(i)))
                 .ToDictionary((s) => s.Key, (s) => s.Value);
 
             return dic;
