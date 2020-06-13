d1ce51743b6483a379adaba5070041ef3b028a6d
LloydLion <LloydLion2211@gmail.com>
2020-06-13 16:29:22 +0000

    NamedList description

--- Standard Library/Collections/NamedList.cs
+++ Standard Library/Collections/NamedList.cs
@@ -6,12 +6,27 @@ using System.Text;
 
 namespace StandardLibrary.Collections
 {
+    /// <summary>
+    /// 
+    ///     List Collection for named objects
+    /// 
+    /// </summary>
     public class NamedList<T> : List<T>, INamedList<T> where T : INamedObject  
     {
+        /// <summary>
+        /// 
+        ///     Search element by his name
+        /// 
+        /// </summary>
         public T this[string index] { get => GetElementByName(index);
             set { var tmp = this.Select((s) => s.Name == index ? value : s).ToArray();
                 this.Clear(); this.AddRange(tmp); } }
 
+        /// <summary>
+        /// 
+        ///     Gets list element by his name    
+        /// 
+        /// </summary>
         public T GetElementByName(string Name) =>
             this.Where((s) => s.Name == Name).Single();
     }
