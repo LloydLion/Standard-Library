2122b860c9872fc2450d0309a5460661e45d6641
LloydLion <LloydLion2211@gmail.com>
2020-06-29 20:52:15 +0300

    Valueble summary

--- Standard Library/TypeManagment/Valueble.cs
+++ Standard Library/TypeManagment/Valueble.cs
@@ -4,18 +4,43 @@ using System.Text;
 
 namespace StandardLibrary.TypeManagment
 {
+    /// <summary>
+    /// 
+    ///     Do from class struct,
+    ///     Nulluble<> but vice versa
+    /// 
+    /// </summary>
+    /// <typeparam name="T">Type for Valuing</typeparam>
     public class Valueble<T> where T : class, ICloneable
     {
         private Valueble() { }
 
         private T Reference { get; set; }
+
+        /// <summary>
+        /// 
+        ///     Cloned input value
+        /// 
+        /// </summary>
         public T Value { get => this; }
 
+        /// <summary>
+        /// 
+        ///     Analog of a.Value
+        /// 
+        /// </summary>
+        /// <param name="a">Input object</param>
         public static implicit operator T(Valueble<T> a)
         {
             return (T)a.Reference.Clone();
         }
 
+        /// <summary>
+        /// 
+        ///     Creates Valueble instance 
+        /// 
+        /// </summary>
+        /// <param name="a">Input reference</param>
         public static implicit operator Valueble<T>(T a)
         {
             return new Valueble<T> { Reference = a };
