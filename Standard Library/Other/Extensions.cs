6e19807048173b7dcbdc2119929a400ebaad0027
LloydLion <LloydLion2211@gmail.com>
2020-08-16 18:03:34 +0300

    Utils and Extensions

---
+++ Standard Library/Other/Extensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StandardLibrary.Other
+{
+	public static class Extensions
+	{
+		public static void InvokeForAll<T>(this IEnumerable<T> obj, Action<T> action)
+		{
+			foreach (var item in obj)
+			{
+				action.Invoke(item);
+			}
+		}
+
+
+		public static long IndexOfLong<T>(this IEnumerable<T> obj, T item) => IndexOfLong(obj, item, EqualityComparer<T>.Default);
+
+		public static long IndexOf<T>(this IEnumerable<T> obj, T item) => IndexOfLong(obj, item, EqualityComparer<T>.Default);
+		
+		public static long IndexOf<T>(this IEnumerable<T> obj, T item, IEqualityComparer<T> comparer) => IndexOfLong(obj, item, comparer);
+
+		public static long IndexOfLong<T>(this IEnumerable<T> obj, T item, IEqualityComparer<T> comparer)
+		{
+			using (var e = obj.GetEnumerator())
+			{
+				long i = 0;
+
+				while (e.MoveNext())
+				{
+					if (comparer.Equals(e.Current, item))
+					{
+						return i;
+					}
+
+					i++;
+				}
+
+				//i--;
+			}
+
+			return -1;
+		}
+	}
+}
