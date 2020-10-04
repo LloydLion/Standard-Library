460ce48ffd0c674e489ee00aa81df0f9f917ccaa
LloydLion <55992792+LloydLion@users.noreply.github.com>
2020-10-04 18:16:34 +0300

    Department Properties

---
+++ Standard Library/Data/Department/DepartmentPropertyInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StandardLibrary.Data.Department
+{
+	public class DepartmentPropertyInfo<TProperty> : DepartmentPropertyInfo where TProperty : class
+	{
+		public DepartmentPropertyInfo(string name) : base(name, typeof(TProperty)) {  }
+
+		public DepartmentPropertyInfo(string name, Type target) : base(name, typeof(TProperty), target) {  }
+	}
+
+
+	public abstract class DepartmentPropertyInfo
+	{
+		public Type PropertyType { get; }
+		public string Name { get; }
+		public Type TargetType { get; }
+		public bool IsReadonly { get; private set; }
+
+
+		protected DepartmentPropertyInfo(string name, Type value)
+		{
+			Name = name;
+			PropertyType = value;
+			TargetType = typeof(object);
+		}
+
+		protected DepartmentPropertyInfo(string name, Type value, Type target) : this(name, value)
+		{
+			TargetType = target;
+		}
+
+
+		public DepartmentPropertyInfo AsReadonly()
+		{
+			IsReadonly = true;
+			return this;
+		}
+	}
+}
---
+++ Standard Library/Data/Department/IDepartmentPropertiesSupport.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StandardLibrary.Data.Department
+{
+	public interface IDepartmentPropertiesSupport
+	{
+		LocalDepartmentPropertiesValuesStore DepartmentStore { get; }
+	}
+}
